4c39e09400247eadc4b72034450f4d5dfb7d73a9
camussumac <136760018+camussumac@users.noreply.github.com>
2025-01-02 22:39:44 -0600

    Update Vite config

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,10 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Legal-Advise-/',
   plugins: [react()],
+  base: './',
   build: {
     outDir: 'dist',
-    sourcemap: true
-  }
+    assetsDir: 'assets',
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
 })
